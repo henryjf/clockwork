deaff138b7d2d5b6f0e604b547f01bcb046a09f9
JEFF HENRY <henry_jf@hotmail.com>
2016-03-07 21:43:43 -0500

    deleted commented coding

--- js/main.js
+++ js/main.js
@@ -1,35 +1,4 @@
-// function getNewDate(){
-// 	return new Date()
-// }
- 
-// var clock = new Date();
-// var secs = clock.getSeconds();
 
-
-// console.log(secs);
-
-// myDate.getSeconds();
-
-// var intervalID = window.setInterval(getNewSeconds, 1000);
-
-// function getNewSeconds(){
-// 	return (new Date().getSeconds())
-// }
-
-// var intervalID = window.setInterval(getNewMinutes, 60000);
-
-// function getNewMinutes() {
-// 	return (new Date().getMinutes())
-// }
-
-// var intervalID = window.setInterval(getNewHours, 3600000)
-
-// function getNewHours() {
-// 	return (new Date().getHours())
-// }
-
-// var clock1 = document.querySelector(".clock1");
-// clock1.innerHTML = (getNewHours) + ":" + (getNewMinutes) + ":" + (getNewSeconds);
 
 
 function getNewTime() {
